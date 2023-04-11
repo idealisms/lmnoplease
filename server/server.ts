1cb4049d1aa8fceac4bef16566b8916c0ae6f1eb
Tony Chang <tony@ponderer.org>
2023-04-11 16:06:27 -0700

    Include libraries for Twitch login.
    Start setting up for authenticating the user following
    the example code here:
    https://github.com/twitchdev/authentication-node-sample

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,16 @@ import express from "express";
 import { postMessages, putMessage } from "./routes/messages";
 import { listPuzzles, getPuzzle } from "./routes/puzzles";
 import { getUser } from "./routes/users";
+import session from "express-session";
+import passport from "passport";
+import { OAuth2Strategy } from "passport-oauth";
+import request from "request";
+import { TWITCH_SECRET, SESSION_SECRET } from "../secrets";
+
+// Configure theses and the secrets.ts file.
+const hostname = "http://localhost:5000";
+const TWITCH_CLIENT_ID = "zjps40uq7gzcnxix0gaot92wca5wn5";
+const CALLBACK_URL = `${hostname}/auth/twitch/callback`;
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,7 +20,9 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// TODO: Authentication. Probably using express-jwt.
+app.use(session({secret: SESSION_SECRET, resave: false, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // user
 app.get("/api/users/:id", getUser);
